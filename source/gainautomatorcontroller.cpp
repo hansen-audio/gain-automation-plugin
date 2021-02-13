eb6196350e4079e002026ee79e7589c25bf2aec1
René Hansen <rene@hansen-audio.de>
2021-02-13 12:34:25 +0100

    Initial commit

---
+++ source/gainautomatorcontroller.cpp
@@ -0,0 +1,106 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2021 Hansen Audio.
+//------------------------------------------------------------------------
+
+#include "gainautomatorcontroller.h"
+#include "gainautomatorcids.h"
+#include "vstgui/plugin-bindings/vst3editor.h"
+
+using namespace Steinberg;
+
+namespace HA {
+
+//------------------------------------------------------------------------
+// GainAutomatorController Implementation
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::initialize (FUnknown* context)
+{
+	// Here the Plug-in will be instanciated
+
+	//---do not forget to call parent ------
+	tresult result = EditControllerEx1::initialize (context);
+	if (result != kResultOk)
+	{
+		return result;
+	}
+
+	// Here you could register some parameters
+
+	return result;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::terminate ()
+{
+	// Here the Plug-in will be de-instanciated, last possibility to remove some memory!
+
+	//---do not forget to call parent ------
+	return EditControllerEx1::terminate ();
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::setComponentState (IBStream* state)
+{
+	// Here you get the state of the component (Processor part)
+	if (!state)
+		return kResultFalse;
+
+	return kResultOk;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::setState (IBStream* state)
+{
+	// Here you get the state of the controller
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::getState (IBStream* state)
+{
+	// Here you are asked to deliver the state of the controller (if needed)
+	// Note: the real state of your plug-in is saved in the processor
+
+	return kResultTrue;
+}
+
+//------------------------------------------------------------------------
+IPlugView* PLUGIN_API GainAutomatorController::createView (FIDString name)
+{
+	// Here the Host wants to open your editor (if you have one)
+	if (FIDStringsEqual (name, Vst::ViewType::kEditor))
+	{
+		// create your editor here and return a IPlugView ptr of it
+		auto* view = new VSTGUI::VST3Editor (this, "view", "gainautomatoreditor.uidesc");
+		return view;
+	}
+	return nullptr;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::setParamNormalized (Vst::ParamID tag, Vst::ParamValue value)
+{
+	// called by host to update your parameters
+	tresult result = EditControllerEx1::setParamNormalized (tag, value);
+	return result;
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::getParamStringByValue (Vst::ParamID tag, Vst::ParamValue valueNormalized, Vst::String128 string)
+{
+	// called by host to get a string for given normalized value of a specific parameter
+	// (without having to set the value!)
+	return EditControllerEx1::getParamStringByValue (tag, valueNormalized, string);
+}
+
+//------------------------------------------------------------------------
+tresult PLUGIN_API GainAutomatorController::getParamValueByString (Vst::ParamID tag, Vst::TChar* string, Vst::ParamValue& valueNormalized)
+{
+	// called by host to get a normalized value from a string representation of a specific parameter
+	// (without having to set the value!)
+	return EditControllerEx1::getParamValueByString (tag, string, valueNormalized);
+}
+
+//------------------------------------------------------------------------
+} // namespace HA
